cd2c3c5126dfa91fa9b5e2cf78cf677998ff49fe
Muecahid Yeniguen <m-a-yeniguen@hotmail.com>
2023-06-11 16:18:43 +0200

    Add bug by default settings in car config

--- sample5/car_config/car_config.js
+++ sample5/car_config/car_config.js
@@ -2,125 +2,163 @@ const SPLIT_FIRST_UNDERSCORE = /_(.*)/s;
 const HTML_ENDING = ".html";
 const SLASH = "/";
 const GO_UP_ONE_DIR = "../";
-const carFirstLetter = 'abcdefghijklmnopqrstuvwxyz'.split('');
-let i = 0;
-let definedCars = [];
-const carProperties = ["Tire", "Interior", "Engine", "Valid", "Name"];
-let counterDisplayCarProp = 0;
 let basedCar = "";
-const DIV = "_div"
+const DIV = "_div";
 let selectedTire = "";
 let selectedInterior = "";
 let selectedEngine = "";
 
+const carProperties = ["Tire", "Interior", "Engine", "Valid", "Name"];
+const aTire = ["20", "22"];
+const aInterior = ["modern", "vintage"];
+const aEngine = ["Combustion Engine A", "Combustion Engine C"];
+const aValid = [true];
+const aName = ["Car A"];
+
+const bTire = ["18", "19", "20"];
+const bInterior = ["modern", "sport"];
+const bEngine = ["Electric Motor A", "Electric Motor B"];
+const bValid = [true];
+const bName = ["Car B"];
+
+const cTire = ["19", "20", "22"];
+const cInterior = ["vintage", "sport"];
+const cEngine = [
+  "Combustion Engine B",
+  "Combustion Engine C",
+  "Electric Motor A",
+  "Electric Motor B",
+];
+const cValid = [true];
+const cName = ["Car C"];
+
+const carFirstLetter = "abcdefghijklmnopqrstuvwxyz".split("");
+const userDefinedCar = createCar();
+
 getValidCars();
 addValidCars();
 
+function createCar() {
+  let standardCars = [];
+  standardCars.splice(0, standardCars.length);
+
+  carFirstLetter.forEach((letter) => {
+    try {
+      if (eval(letter.concat(carProperties[0])) != "undefined") {
+        carSpecificProp = carProperties.map((prop, i) => [
+          prop,
+          [
+            eval(letter.concat(prop)),
+            eval(letter.concat(prop)),
+            eval(letter.concat(prop)),
+            eval(letter.concat(prop)),
+            eval(letter.concat(prop)),
+          ][i],
+        ]);
+        standardCars.push(new Map(carSpecificProp));
+      }
+    } catch (error) {
+      //not defined car properties
+    }
+  });
+  return standardCars;
+}
 
 function changeFunctionality(btn) {
-    let htmpage = btn.id.split(SPLIT_FIRST_UNDERSCORE)[1];
-    path = location.pathname;
-    location.href = GO_UP_ONE_DIR + htmpage + SLASH + htmpage + HTML_ENDING;
-
-
+  let htmpage = btn.id.split(SPLIT_FIRST_UNDERSCORE)[1];
+  path = location.pathname;
+  location.href = GO_UP_ONE_DIR + htmpage + SLASH + htmpage + HTML_ENDING;
 }
 
 function getValidCars() {
-    for (let carNum = 0; carNum < carFirstLetter.length; carNum++) {
-        carArrays = JSON.parse(localStorage.getItem(carFirstLetter[carNum].toUpperCase()));
-        if (carArrays) {
-            definedCars[i] = new Map(carArrays);
-            i++;
-        }
-    }
-    console.log("Valid:");
-    console.log(definedCars);
-
+  for (let carNum = 0; carNum < userDefinedCar.length; carNum++) {
+    let carName = userDefinedCar[carNum].get(carProperties[4])[0];
+    carArrays = localStorage.getItem(carName);
+    userDefinedCar[carNum].set(carProperties[3], eval(carArrays));
+    console.log(userDefinedCar);
+  }
 }
 
 function addValidCars() {
-    selectorModel = document.getElementById("Model");
-    selectorModel.length = 0;
-    for (let carNum = 0; carNum < definedCars.length; carNum++) {
-        option = document.createElement("option");
-        option.text = definedCars[carNum].get(carProperties[4]);
-        selectorModel.add(option);
+  selectorModel = document.getElementById("Model");
+  selectorModel.length = 0;
+  for (let carNum = 0; carNum < userDefinedCar.length; carNum++) {
+    if (userDefinedCar[carNum].get(carProperties[3])) {
+      option = document.createElement("option");
+      option.text = userDefinedCar[carNum].get(carProperties[4]);
+      selectorModel.add(option);
     }
+  }
 }
 
 function selectedCar() {
-    selectorModel = document.getElementById("Model");
-    carName = selectorModel.options[selectorModel.selectedIndex].text;
+  selectorModel = document.getElementById("Model");
+  carName = selectorModel.options[selectorModel.selectedIndex].text;
 
-    for (let carNum = 0; carNum < definedCars.length; carNum++) {
-        if (definedCars[carNum].get(carProperties[4]) == carName) {
-            basedCar = definedCars[carNum];
-            break;
-        }
+  for (let carNum = 0; carNum < userDefinedCar.length; carNum++) {
+    if (userDefinedCar[carNum].get(carProperties[4]) == carName) {
+      basedCar = userDefinedCar[carNum];
+      break;
     }
-    resetConfig()
+  }
+  resetConfig();
 }
 
 function addOption(actualSelector) {
-    selector = document.getElementById(actualSelector);
-    addProperties = basedCar.get(actualSelector);
+  selector = document.getElementById(actualSelector);
+  addProperties = basedCar.get(actualSelector);
 
-    if (document.getElementById(actualSelector + DIV).style.display == "none") {
-        selector.length = 0;
-        for (let prop = 0; prop < addProperties.length; prop++) {
-            option = document.createElement("option");
-            option.text = addProperties[prop];
-            selector.add(option);
-        }
+  if (document.getElementById(actualSelector + DIV).style.display == "none") {
+    selector.length = 0;
+    for (let prop = 0; prop < addProperties.length; prop++) {
+      option = document.createElement("option");
+      option.text = addProperties[prop];
+      selector.add(option);
     }
+  }
 }
 
 function displayPropConfig(element) {
-    nextConfigDisplay = document.getElementById(element + DIV);
-    nextConfigDisplay.style.display = "block";
+  nextConfigDisplay = document.getElementById(element + DIV);
+  nextConfigDisplay.style.display = "block";
 }
 
 function displayConfigBtn() {
-    configBtn = document.getElementById("btn_display_config");
-    configBtn.style.display = "block";
+  configBtn = document.getElementById("btn_display_config");
+  configBtn.style.display = "block";
 }
 
-
 function deleteOptions() {
-    for (let propNum = 0; propNum < carProperties.length - 2; propNum++) {
-        document.getElementById(carProperties[propNum]).length = 0;
-
-    }
+  for (let propNum = 0; propNum < carProperties.length - 2; propNum++) {
+    document.getElementById(carProperties[propNum]).length = 0;
+  }
 }
 
 function getNextConfig(element) {
-    elementName = element.id;
-    configs = document.getElementsByClassName('car_config_selec');
-    for (let i = 0; i < configs.length; i++) {
-        if (configs[i].id == elementName) {
-            return configs[i + 1].id;
-        }
+  elementName = element.id;
+  configs = document.getElementsByClassName("car_config_selec");
+  for (let i = 0; i < configs.length; i++) {
+    if (configs[i].id == elementName) {
+      return configs[i + 1].id;
     }
+  }
 }
 
 function displayResultConfig() {
-    resultConfig = [];
-    resultConfig.push([["Selected Car"], basedCar.get(carProperties[4])]);
-    for (let propNum = 0; propNum < carProperties.length - 2; propNum++) {
-        selector = document.getElementById(carProperties[propNum]);
-        value = selector.options[selector.selectedIndex].text;
-        resultConfig.push([[carProperties[propNum]], [value]])
-    }
-    console.log(resultConfig)
-} 
+  resultConfig = [];
+  resultConfig.push([["Selected Car"], basedCar.get(carProperties[4])]);
+  for (let propNum = 0; propNum < carProperties.length - 2; propNum++) {
+    selector = document.getElementById(carProperties[propNum]);
+    value = selector.options[selector.selectedIndex].text;
+    resultConfig.push([[carProperties[propNum]], [value]]);
+  }
+}
 
-function resetConfig(){
-    for (let propNum = 0; propNum < carProperties.length-2; propNum++) {
-        configProp = document.getElementById(carProperties[propNum] + DIV);
-        configProp.style.display = "none";
-    }
-    configBtn = document.getElementById("btn_display_config");
-    configBtn.style.display = "none";
-
-
+function resetConfig() {
+  for (let propNum = 0; propNum < carProperties.length - 2; propNum++) {
+    configProp = document.getElementById(carProperties[propNum] + DIV);
+    configProp.style.display = "none";
+  }
+  configBtn = document.getElementById("btn_display_config");
+  configBtn.style.display = "none";
 }
