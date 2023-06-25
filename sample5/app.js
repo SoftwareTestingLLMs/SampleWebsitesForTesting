65761a1fcc2c099cb0437e7160e9a8c2f23b362b
Muecahid Yeniguen <m-a-yeniguen@hotmail.com>
2023-06-25 16:03:16 +0200

    Fix bug of css, working with bootstrap

--- sample5/app.js
+++ sample5/app.js
@@ -10,12 +10,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require('path');
+
 app.set("view engine","ejs")
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 console.log(__dirname+"\n")
 
-app.get("/css", function (req, res) {
+app.get("/", function (req, res) {
   res.render("index")
 });
 
