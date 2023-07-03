aa5a13fa05bb06ecb38d08b1279ffd40ef21ef48
Muecahid Yeniguen <m-a-yeniguen@hotmail.com>
2023-07-03 15:32:51 +0200

    Fix bug txt printer Display latest values instead latest displayed values

--- sample5/public/js/main/txt_printer.js
+++ sample5/public/js/main/txt_printer.js
@@ -1,6 +1,3 @@
-let isTextPrinted = sessionStorage.getItem("isTxtPrinted")
-  ? sessionStorage.getItem("isTxtPrinted")
-  : false;
 const BRACE_LEFT = "[";
 const BRACE_RIGHT = "]";
 const LINE_BREAK = "<br>";
@@ -9,21 +6,73 @@ const stateFigPrinter = sessionStorage.getItem("stateFigPrinter")
   : "hidden";
 
 isFigPrinterActivated();
-maintainPrinting();
+printedText();
 
 function isFigPrinterActivated() {
   document.getElementById("btn_fig_printer").style.visibility = stateFigPrinter;
 }
 
-function maintainPrinting() {
-  console.log(isTextPrinted);
-  if (isTextPrinted) {
-    printText();
-  }
+function updateText() {
+  sessionStorage.setItem("isTxtPrinted", "true");
+  printText();
 }
 
 function printText() {
-  isTextPrinted = sessionStorage.setItem("isTxtPrinted", true);
+  readSettings();
+  if (sessionStorage.getItem("isTxtPrinted")) {
+    document.getElementById("txt_content").innerHTML =
+      "Font Styles:" +
+      BRACE_LEFT +
+      style +
+      BRACE_RIGHT +
+      LINE_BREAK +
+      "Color: Color." +
+      color.toUpperCase() +
+      LINE_BREAK +
+      "Font Size: " +
+      fontSize +
+      LINE_BREAK +
+      "Font: " +
+      font +
+      LINE_BREAK +
+      "Number of words: " +
+      numWords;
+
+    maintainPrintedValues();
+  }
+}
+
+function printedText() {
+  if (sessionStorage.getItem("isTxtPrinted")) {
+    document.getElementById("txt_content").innerHTML =
+      "Font Styles:" +
+      BRACE_LEFT +
+      JSON.parse(sessionStorage.getItem("printedSytle")) +
+      BRACE_RIGHT +
+      LINE_BREAK +
+      "Color: Color." +
+      sessionStorage.getItem("printedColor").toUpperCase() +
+      LINE_BREAK +
+      "Font Size: " +
+      sessionStorage.getItem("printedFontSize") +
+      LINE_BREAK +
+      "Font: " +
+      sessionStorage.getItem("printedFont") +
+      LINE_BREAK +
+      "Number of words: " +
+      sessionStorage.getItem("printedNumWords");
+  }
+}
+
+function maintainPrintedValues() {
+  sessionStorage.setItem("printedSytle", JSON.stringify(style));
+  sessionStorage.setItem("printedColor", color);
+  sessionStorage.setItem("printedFontSize", fontSize);
+  sessionStorage.setItem("printedFont", font);
+  sessionStorage.setItem("printedNumWords", numWords);
+}
+
+function readSettings() {
   style = JSON.parse(sessionStorage.getItem("style"))
     ? JSON.parse(sessionStorage.getItem("style"))
     : [];
@@ -39,22 +88,4 @@ function printText() {
   numWords = sessionStorage.getItem("numWords")
     ? sessionStorage.getItem("numWords")
     : "50";
-
-  document.getElementById("txt_content").innerHTML =
-    "Font Styles:" +
-    BRACE_LEFT +
-    style +
-    BRACE_RIGHT +
-    LINE_BREAK +
-    "Color: Color." +
-    color.toUpperCase() +
-    LINE_BREAK +
-    "Font Size: " +
-    fontSize +
-    LINE_BREAK +
-    "Font: " +
-    font +
-    LINE_BREAK +
-    "Number of words: " +
-    numWords;
 }
