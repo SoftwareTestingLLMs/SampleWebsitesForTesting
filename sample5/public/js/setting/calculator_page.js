7de761c41384a0acce21b66875e9b9f9b5b25ab3
Muecahid Yeniguen <m-a-yeniguen@hotmail.com>
2023-07-07 15:52:52 +0200

    Add maintain state calculator page

--- sample5/public/js/setting/calculator_page.js
+++ sample5/public/js/setting/calculator_page.js
@@ -1,12 +1,11 @@
-const DEFAULT_COLOR = "black";
-const DEFAULT_STYLE = "";
+const selectNumSys = document.getElementById("num_sys");
 
-function openSettingPage(btn) {}
+maintainState();
 
 function checkReq() {
   selectionArithmetic = document.querySelectorAll(
     'input[name="arithmetic"]:checked'
-  ); 
+  );
   if (selectionArithmetic.length == 0) {
     $("#modalMinimu").modal("show");
     $("#btn_modal_close").on("click", function () {
@@ -23,13 +22,14 @@ function checkReq() {
     });
   }
 }
-
-function closeSettings() {
+function saveNumSys() {
   //Save numeral system in storage
   selectorSys = document.getElementById("num_sys");
   numSys = selectorSys.options[selectorSys.selectedIndex].value;
   sessionStorage.setItem("numSys", numSys);
+}
 
+function saveAritmetic() {
   //Save arithmetic in storage
   selectionArithmetic = document.querySelectorAll(
     'input[name="arithmetic"]:checked'
@@ -39,4 +39,20 @@ function closeSettings() {
     selectedArithmetic.push(element.id);
   });
   sessionStorage.setItem("arithmetic", JSON.stringify(selectedArithmetic));
+}
+
+function maintainState() {
+  aritmethic = JSON.parse(sessionStorage.getItem("arithmetic"))
+    ? JSON.parse(sessionStorage.getItem("arithmetic"))
+    : ["addition"];
+  numSys = sessionStorage.getItem("num_sys")
+    ? sessionStorage.getItem("num_sys")
+    : "decimal";
+
+  selectNumSys.value = numSys;
+
+
+  for (let arithmeticNum = 0; arithmeticNum < aritmethic.length; arithmeticNum++) {
+    document.getElementById(aritmethic[arithmeticNum]).checked = true;
+  }
 }
